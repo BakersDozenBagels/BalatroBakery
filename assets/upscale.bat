6f89d46bd0c9261b6790c1a9ddfa541ca83653ec
BakersDozenBagels <bakersdozenbagelsbergman@gmail.com>
2026-04-27 12:12:04 -0400

    Update upscale scripts

--- assets/upscale.bat
+++ assets/upscale.bat
@@ -1,1 +1,18 @@
-for /F %%x in ('dir /B/D 1x') do magick 1x\%%x -filter point -resize 200%% 2x\%%x
+@echo off
+for /F %%x in ('dir /B/D 2x') do (
+    if %%~xx == .png (
+        if not exist 1x\%%x (
+            echo Removing deleted file 1x\%%x from 2x\
+            del 2x\%%x
+        )
+    )
+)
+for /F %%x in ('dir /B/D 1x') do (
+    if %%~xx == .png (
+        REM https://stackoverflow.com/questions/1687014/how-do-i-compare-timestamps-of-files-in-a-batch-script/58323817#58323817
+        xcopy /LDY /-I 1x\%%x 2x\%%x | findstr /B /C:"1 " > nul 2>&1 && (
+            echo Updating changed file 1x\%%x in 2x\
+            magick 1x\%%x -filter point -resize 200%% 2x\%%x
+        )
+    )
+)
--- assets/upscale.sh
+++ assets/upscale.sh
@@ -1,6 +1,17 @@
 #!/bin/sh
+for file in 2x/*.png; do
+    filename=$(basename "$file")
+    if [ ! -f "1x/${filename}" ]; then
+        echo "Removing deleted file ${file} from 2x"
+        rm "2x/${filename}"
+    fi
+done
+
 for file in 1x/*.png; do
     filename=$(basename "$file")
     output_file="2x/${filename}"
-    magick "$file" -filter point -resize 200% "$output_file"
+    if [ ! -f "$output_file" ] || [ "$file" -nt "$output_file" ]; then
+        echo "Updating changed file ${file} in 2x/"
+        magick "$file" -filter point -resize 200% "$output_file"
+    fi
 done
