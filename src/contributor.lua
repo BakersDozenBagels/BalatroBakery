01ad7d31ac60e70aaac2bb2520d44dd39ec14707
BakersDozenBagels <bakersdozenbagelsbergman@gmail.com>
2026-06-06 13:00:29 -0400

    Add annotation

--- src/contributor.lua
+++ src/contributor.lua
@@ -1,36 +1,37 @@
 -- KEEP_LITE
 Bakery_API.guard(function()
-    Bakery_API.contributors = {
-        GhostSalt = {
-            name = "GhostSalt",
-            fg = HEX 'FFFFFF',
-            bg = HEX '4D1575'
-        },
-        SadCube = {
-            name = "SadCube",
-            fg = G.C.UI.TEXT_DARK,
-            bg = HEX '1ddebe'
-        },
-        ClausStephan = {
-            name = "Claus Stephan",
-            bg = HEX 'AF8BBB',
-            fg = HEX '000000'
-        },
-        AmyWeber = {
-            name = "Amy Weber",
-            bg = HEX '60ADC4',
-            fg = G.C.UI.TEXT_DARK
-        },
-        Jack5 = {
-            name = "Jack5",
-            bg = HEX 'ff4d00',
-            fg = HEX 'ffffff'
-        },
-        Craw = {
-            name = "https://craw.run",
-            bg = HEX 'FF2862',
-            fg = HEX 'ffffff'
-        }
-    }
+	---@type table<string, {name: string, fg: table, bg: table}>
+	Bakery_API.contributors = {
+		GhostSalt = {
+			name = "GhostSalt",
+			fg = HEX("FFFFFF"),
+			bg = HEX("4D1575"),
+		},
+		SadCube = {
+			name = "SadCube",
+			fg = G.C.UI.TEXT_DARK,
+			bg = HEX("1ddebe"),
+		},
+		ClausStephan = {
+			name = "Claus Stephan",
+			bg = HEX("AF8BBB"),
+			fg = HEX("000000"),
+		},
+		AmyWeber = {
+			name = "Amy Weber",
+			bg = HEX("60ADC4"),
+			fg = G.C.UI.TEXT_DARK,
+		},
+		Jack5 = {
+			name = "Jack5",
+			bg = HEX("ff4d00"),
+			fg = HEX("ffffff"),
+		},
+		Craw = {
+			name = "https://craw.run",
+			bg = HEX("FF2862"),
+			fg = HEX("ffffff"),
+		},
+	}
 end)
 -- END_KEEP_LITE
